4f4be141ef70a7c9fdd69b381803e9a4092c0dc5
Manuk <Manuk@DESKTOP-UC6MOHF>
2023-05-22 17:39:11 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+Console.Write("Hello, World,Manuk");
+//Console.ReadKey();
+//Console.Beep();
+
+ float t = 25;
+Console.WriteLine(t);
+{
+int apple = 5;
+int banan = 3;
+
+int fruits = apple + banan;
+Console.WriteLine(fruits);
+}
+
+//int size = sizeof(decimal);
+//Console.WriteLine(size);
+
+{
+    byte augend = 10, added = 2;
+    int sum = augend + added;
+
+    byte minuend = 10, subtrahend = 3;
+    int difference = minuend - subtrahend;
+
+    byte multiplicand = 100, multiplier = 3;
+    int product = multiplicand * multiplier;
+
+    byte dividend = 13, divisor = 2;
+    int quotient = dividend / divisor;
+    int remainder = dividend % divisor;
+
+    Console.WriteLine(sum);
+    Console.WriteLine(difference);
+    Console.WriteLine(product);
+    Console.WriteLine(quotient);
+    Console.WriteLine(remainder);
+
+}
+{
+    byte apples = 16;
+    byte pears = 16;
+
+    byte fruits = (byte)(apples + pears);
+    Console.WriteLine(fruits);
+}
+
+{
+    bool a = true, b = false;
+    Console.WriteLine($"Inst: {a}, logh: {b}");
+
+}
+
+{
+    int x = 2, y = 3;
+
+    bool isEqual = x == y;
+    Console.WriteLine($"{x} == {y} = {isEqual}");
+
+    bool isNotEqual = x != y;
+    Console.WriteLine($"{x}!= {y}={isNotEqual}");
+
+    bool isLess = x < y;
+    Console.WriteLine($"{x} < {y}={isLess}");
+
+}
+{
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+    int result = a + b + c + d;
+    Console.WriteLine(result);
+}
